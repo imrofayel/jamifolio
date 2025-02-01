53ee81ff6d99726f3dfb4d9aa45e05c2aebd36b4
rofayels <adamrofayel@gmail.com>
2025-02-01 15:20:41 +0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="flex w-full h-screen justify-center items-center">
+
+    <h1 class="blur-[.3px]">Mahraib Fatima</h1>
+
+    <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200" height="200" />
+
+    <div class="grid sm:grid-cols-8 grid-cols-4 sm:grid-rows-20 gap-2 overflow-hidden">
+      <div class="elem col-span-1 row-span-1" style="--r: 1; --c: 3;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 1; --c: 7;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 2; --c: 2;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 2; --c: 6;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 3; --c: 4;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 3; --c: 8;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 4; --c: 1;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 4; --c: 5;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 5; --c: 3;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 5; --c: 7;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 6; --c: 2;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 6; --c: 6;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 7; --c: 4;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 7; --c: 8;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 8; --c: 1;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 8; --c: 5;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 9; --c: 3;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 9; --c: 7;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 10; --c: 2;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl -z-10 absolute" width="200"
+          height="200" />
+      </div>
+      <div class="elem col-span-1 row-span-1" style="--r: 10; --c: 6;">
+        <NuxtImg src="img/flowers2.webp" class="blur-[1px] bg-flower rounded-6xl" width="200" height="200" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
